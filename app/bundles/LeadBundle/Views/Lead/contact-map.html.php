cccccb0e441200c832be78a1d2cc4b8fbe91662b
Brian Wider <brian.x.wider.-nd@disney.com>
2017-03-11 20:28:30 -0300

    segments

--- app/bundles/LeadBundle/Views/Lead/contact-map.html.php
+++ app/bundles/LeadBundle/Views/Lead/contact-map.html.php
@@ -118,8 +118,8 @@ foreach ($tags as $tag) {
     <script src="http://63.141.233.213/prueba2/demo4/leaflet-markercluster.js"></script>
     <script src="http://63.141.233.213/prueba2/demo4/leaflet-image.js"></script>
 <?php
-require __DIR__.'/../vendor/modules/spin.min.js';
-require __DIR__.'/../vendor/modules/leaflet.spin.min.js';
+echo $view['assets']->includeScript('vendor/modules/spin.min.js';
+echo $view['assets']->includeScript('vendor/modules/leaflet.spin.min.js';
 ?>
 
 <script type="text/javascript">
